278433005c1a4e31f5290ffc2ae3e445a795ca74
DSMIXRJ <dsmixrj@gmail.com>
2025-05-02 17:25:01 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,24 +5,17 @@ const path = require('path');
 const router = express.Router();
 
 router.post('/login', (req, res) => {
-  let body = '';
-  req.on('data', chunk => (body += chunk));
-  req.on('end', () => {
-    try {
-      const { email, password } = JSON.parse(body);
-      const usersPath = path.join(__dirname, '..', 'users.json');
-      const users = JSON.parse(fs.readFileSync(usersPath, 'utf8'));
-      const user = users.find(u => u.email === email && u.password === password);
+  const { email, password } = req.body;
 
-      if (user) {
-        res.status(200).json({ message: 'Login bem-sucedido' });
-      } else {
-        res.status(401).json({ message: 'Credenciais inválidas' });
-      }
-    } catch (error) {
-      res.status(500).json({ message: 'Erro no servidor' });
-    }
-  });
+  const usersPath = path.join(__dirname, '..', 'users.json');
+  const users = JSON.parse(fs.readFileSync(usersPath, 'utf8'));
+  const user = users.find(u => u.email === email && u.password === password);
+
+  if (user) {
+    res.status(200).json({ message: 'Login bem-sucedido' });
+  } else {
+    res.status(401).json({ message: 'Credenciais inválidas' });
+  }
 });
 
 module.exports = router;
