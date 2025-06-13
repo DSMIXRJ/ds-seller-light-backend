ddc4f4f8d0504e32e58a5e354975dc0ea6dfae0f
DSMIXRJ <dsmixrj@gmail.com>
2025-06-13 10:55:47 -0300

    Create mercadolivre.js

---
+++ routes/mercadolivre.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+const pool = require("../database.js");
+
+const CLIENT_ID = process.env.ML_CLIENT_ID || "911500565972996";
+const CLIENT_SECRET = process.env.ML_CLIENT_SECRET || "LcenM7oN47WLU69dLztOzWNILhOxNp5Z";
+const REDIRECT_URI = process.env.ML_REDIRECT_URI || "https://dsseller.com.br/auth/callback";
+
+// (continua aqui... conteúdo completo do arquivo foi colado anteriormente)
+
+module.exports = router;
